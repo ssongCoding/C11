34541985d315541a92ede924681f978b27c47350
ssongCoding <105415016+ssongCoding@users.noreply.github.com>
2022-07-29 18:41:20 +0900

    Add files via upload

---
+++ Day18/Day18_EX1.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//struct Score { // c++ 
+//	int midExam;
+//	int finalExam;
+//};
+//
+//int main() {
+//	// Score º¯¼ö s1 = 100, 0
+//	struct Score s1 = { 100, 0 };
+//
+//	int ave = (s1.midExam + s1.finalExam) / 2;
+//	printf("%d", ave);
+//
+//	return 0;
+//}
---
+++ Day18/Day18_EX2.c
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+////#define seven 8 // 컴퓨터가 코드 실행 전에
+//			    // seven을 하나하나 찾아가서
+//				// 다 값을 8로 바꿔버릴 거에요.
+//
+//int main() {
+//
+//	int num = 3;
+//	const int seven = 8; // 상수 : 변하지 않는 수
+//			         	 // 코드를 실행하는 동안
+//	printf("%d", num * 8);
+//
+//	num = 5;
+//	// seven = 10; 이게 안됨.
+//	printf("%d", num * seven);
+//
+//	num = 10;
+//	printf("%d", num * seven);
+//
+//	num = 100;
+//	printf("%d", num * seven);
+//
+//	num = 4;
+//	printf("%d", num * seven);
+//
+//	return 0;
+//}
